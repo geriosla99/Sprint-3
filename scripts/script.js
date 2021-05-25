e3a3cde8ecf88a867b1b6a5048db7a552ccdf748
geriosla99 <geriosla@gmail.com>
2021-05-25 11:24:43 -0500

    sprint3

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,10 @@
- function AddToCar(){
+var productoPrecio = document.getElementsByClassName("productoPrecio")[0].innerHTML;   
+var productoTitulo = document.getElementsByClassName("productoTitulo")[0].innerHTML;
+var productoCantidad = document.getElementsByClassName("productoCantidad")[0].value;  
+var productoImagenUrl = document.getElementsByClassName("productoImagenUrl")[0].src; 
+var productoId = document.getElementsByClassName("productoId")[0].id;
+
+function AddToCar(){
     var productoPrecio = document.getElementsByClassName("productoPrecio")[0].innerHTML;   
     var productoTitulo = document.getElementsByClassName("productoTitulo")[0].innerHTML;
     var productoCantidad = document.getElementsByClassName("productoCantidad")[0].value;  
@@ -11,7 +17,11 @@
     alert("Producto agregado correctamente al carrito"); 
 };
 
+function Car(){
+    localStorage.getItem(productoId);
+}
 
+//formulario
 
 
 
